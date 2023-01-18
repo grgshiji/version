53dc50d37f351ff288709fa753f95d8219ad193f
grgshiji <shiji@neoito.com>
2023-01-18 15:26:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const version = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello version`}))
+  res.end()
+  
+}
+
+export default version
